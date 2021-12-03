bf0d9f68ecd99b33c4ac24b06453db82fa69ac8a
Lazaro1 <lazarolimasilva2@gmail.com>
2021-12-03 19:43:31 -0300

    correct struct for deploy with nextJS

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import * as S from './styles'
+import * as S from 'styles/login'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { schemaLogin } from 'Validators/SingInValidator'
